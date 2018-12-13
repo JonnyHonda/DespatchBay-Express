571b1b9a4f145432c9a92028193018c36cae7970
John Burrin <jonnyb3010@gmail.com>
2018-12-13 20:21:15 +0000

    Add Beep

---
+++ ScanSku/TrackingNumberDataAdapter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace DespatchBayExpress
+{
+    public class TrackingNumberDataAdapter : RecyclerView.Adapter
+    {
+        public BarcodeScannerList mBarcodeScannerList;
+        public TrackingNumberDataAdapter(BarcodeScannerList bcl)
+        {
+            mBarcodeScannerList = bcl;
+        }
+
+        public override RecyclerView.ViewHolder
+            OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.ListViewItem
+                        , parent, false);
+            TrackingNumberViewHolder vh = new TrackingNumberViewHolder(itemView);
+            return vh;
+        }
+
+        public override void
+            OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            TrackingNumberViewHolder vh = holder as TrackingNumberViewHolder;
+            try
+            {
+                vh.Caption.Text = mBarcodeScannerList[position].Caption;
+            }
+            catch { vh.Caption.Text = "Missing Tracking Number"; }
+        }
+
+        public override int ItemCount
+        {
+            get { return mBarcodeScannerList.NumBarcodes; }
+        }
+    }
+}
---
+++ ScanSku/TrackingNumberViewHolder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DespatchBayExpress
+{
+    public class TrackingNumberViewHolder : Android.Support.V7.Widget.RecyclerView.ViewHolder
+    {
+     //   public ImageView Image { get; private set; }
+        public TextView Caption { get; private set; }
+        public TrackingNumberViewHolder(View itemView) : base(itemView)
+        {
+            // Locate and cache view references:
+            Caption = itemView.FindViewById<TextView>(Resource.Id.textView);
+        }
+    }
+}
